9b3bf0c8265539bd2a7c03f3c7429b156309e6d3
saatvik-10 <saatvikmadan529@gmail.com>
2026-07-23 01:49:04 +0530

    echo tool call

---
+++ src/tools/echo.tool.ts
@@ -0,0 +1,17 @@
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp';
+import { EchoService } from '../services/echo.service';
+import { echoSchema } from '../schemas/echo.schema';
+
+export function registerEchoTool(server: McpServer) {
+  server.registerTool(
+    'echo',
+    {
+      title: 'Echo Tool',
+      description: 'Returns the same message back',
+      inputSchema: echoSchema,
+    },
+    async ({ message }) => {
+      return EchoService.echo(message);
+    },
+  );
+}
