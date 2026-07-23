bb3fc2434d1357227d5172d302802a3b71b4467c
saatvik-10 <saatvikmadan529@gmail.com>
2026-07-23 23:02:44 +0530

    github tools inder exporter

---
+++ src/tools/github/index.ts
@@ -0,0 +1,14 @@
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp';
+import { registerCreateIssueTool } from './create-issue.tool';
+import { registerListRepoTool } from './list-repo.tool';
+import { registerRepoDetailsTool } from './repo-details.tool';
+import { registerListIssuesTool } from './list-issue.tool';
+import { registerSearchRepoTool } from './search-repo.tool';
+
+export function registerGithubTools(server: McpServer) {
+  registerCreateIssueTool(server);
+  registerListRepoTool(server);
+  registerRepoDetailsTool(server);
+  registerListIssuesTool(server);
+  registerSearchRepoTool(server);
+}
